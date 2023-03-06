2538cbe7fc10a5c09e1d08df9be98a553b10fc39
kretinina tatiana <kretinina1202@mail.ru>
2023-03-06 18:20:08 +0300

    Added style.css

--- lab_1/index.php
+++ lab_1/index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lab_1</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./style.css">
 </head>
 <body>
     <header class="header">
@@ -20,14 +20,16 @@
                 <?php
                     $img = array("img/1.avif", "img/2.avif", "img/3.avif", "img/4.avif");
                     for ($i = 0; $i < 4; $i++) {
-                        echo "<img src=\"$img[$i]\" width="100">"
+                        echo "<img src=\"$img[$i]\" width='100'>";
                     }
                 ?>
             </div>
         </div>
     </main>
-    <footer>
+    <footer class="footer">
+        <div class="wrapper">
         <p>Создать веб-страницу с динамическим контентом. Загрузить код в удаленный репозиторий. Залить на хостинг.</p>
+        </div>
     </footer>
 </body>
 </html>
